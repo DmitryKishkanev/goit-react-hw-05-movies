1e293726da2ea50e34ee642c6e1662b2bd3da758
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-12-08 02:02:24 +0200

    добавил файл 404.html

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router';
+import { Global, css } from '@emotion/react';
 import Layout from 'components/Layout';
 import { Container } from 'components/App/App.styled';
 
@@ -11,17 +12,28 @@ const Reviews = lazy(() => import('components/Reviews'));
 
 export default function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+    <>
+      {/* Оставляет место с правой стороны под скрол, что бы не дёргался экран  */}
+      <Global
+        styles={css`
+          html {
+            overflow-y: scroll;
+            scrollbar-gutter: stable;
+          }
+        `}
+      />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
-    </Container>
+        </Routes>
+      </Container>
+    </>
   );
 }
